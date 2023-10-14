76513acf27e346dfa9e5bc4d0ce431c53de8a3d8
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-14 19:34:23 +0300

    added handling an exception

--- src/main/java/quiz/service/app/controller/QuestionController.java
+++ src/main/java/quiz/service/app/controller/QuestionController.java
@@ -1,9 +1,7 @@
 package quiz.service.app.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import quiz.service.app.model.Question;
 import quiz.service.app.service.QuestionService;
 
@@ -20,12 +18,17 @@ public class QuestionController {
     }
 
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestion() {
+    public ResponseEntity<List<Question>> getAllQuestion() {
         return questionService.getAllQuestions();
     }
 
     @GetMapping("category/{category}")
     public List<Question> getQuestionsByCategory(@PathVariable String category) {
         return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("add")
+    public String addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
     }
 }
--- src/main/java/quiz/service/app/service/QuestionService.java
+++ src/main/java/quiz/service/app/service/QuestionService.java
@@ -1,9 +1,12 @@
 package quiz.service.app.service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import quiz.service.app.dao.QuestionDao;
 import quiz.service.app.model.Question;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,11 +18,21 @@ public class QuestionService {
         this.questionDao = questionDao;
     }
 
-    public List<Question> getAllQuestions() {
-        return questionDao.findAll();
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
     public List<Question> getQuestionsByCategory(String category) {
         return questionDao.findByCategory(category);
+    }
+
+    public String addQuestion(Question question) {
+        questionDao.save(question);
+        return "success";
     }
 }
