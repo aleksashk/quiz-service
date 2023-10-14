5b875204e93ab8b3ac8174f137241793b41c6de8
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-14 19:40:18 +0300

    added handling an exception everywhere

--- src/main/java/quiz/service/app/controller/QuestionController.java
+++ src/main/java/quiz/service/app/controller/QuestionController.java
@@ -23,12 +23,12 @@ public class QuestionController {
     }
 
     @GetMapping("category/{category}")
-    public List<Question> getQuestionsByCategory(@PathVariable String category) {
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category) {
         return questionService.getQuestionsByCategory(category);
     }
 
     @PostMapping("add")
-    public String addQuestion(@RequestBody Question question) {
+    public ResponseEntity<String> addQuestion(@RequestBody Question question) {
         return questionService.addQuestion(question);
     }
 }
